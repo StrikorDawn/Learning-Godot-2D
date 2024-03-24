912b24487a963a468e4f6a9f0ad9162fc624e456
Taden Marston <113535943+StrikorDawn@users.noreply.github.com>
2024-03-23 21:20:13 -0600

    Fixed some errors

--- scripts/finish.gd
+++ scripts/finish.gd
@@ -2,4 +2,7 @@ extends Area2D
 @export var target_level: PackedScene
 func _on_body_entered(body):
 	if(body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_packed(target_level)
+		call_deferred("change_scene")
+
+func change_scene():
+	get_tree().change_scene_to_packed(target_level)
